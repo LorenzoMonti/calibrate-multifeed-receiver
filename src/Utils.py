3ab29ca015481a9b059bb2b2b19b3b475e1f9025
LorenzoMonti <lorenzomonti42@gmail.com>
2021-12-17 16:31:01 +0100

    Add User Interface and config files
    Added a User Interface in order to support the users. Moreover, configuration file has been created

--- src/Utils.py
+++ src/Utils.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import datetime
-
+import json
   
-def set_SPA_for_measure(ms2830a):
+def set_SPA_for_measure(ms2830a, config_file):
         """
         This method allows to set the Anritsu MS2830A for the measurements
         """    
@@ -14,31 +14,31 @@ def set_SPA_for_measure(ms2830a):
         ms2830a.set_SPA()
         
         # Frequency
-        ms2830a.do_set_startfreq(1000000000) # 1Ghz
-        ms2830a.do_set_stopfreq(4000000000)
-        ms2830a.do_set_centerfreq(2000000000)
+        ms2830a.do_set_startfreq(config_file["start_freq"]) # 1Ghz
+        ms2830a.do_set_stopfreq(config_file["stop_freq"])
+        ms2830a.do_set_centerfreq(config_file["center_freq"])
         print("Start Frequency: " + str(ms2830a.do_get_startfreq()))
         print("Stop Frequency: " + str(ms2830a.do_get_stopfreq()))
         print("Center Frequency: " + str(ms2830a.do_get_centerfreq()))
         
         # Time sweep
-        ms2830a.set_trace_points_sweeptime(5001)
+        ms2830a.set_trace_points_sweeptime(config_file["sweep_trace_points"])
         print("Sweep Trace points: " + str(ms2830a.get_trace_points_sweeptime()))
 
         # BW
-        ms2830a.do_set_resolutionBW(1000000) # 1Mhz
+        ms2830a.do_set_resolutionBW(config_file["resolution_bandwith"]) # 1Mhz
         print("Resolution Bandwith: " + str(ms2830a.do_get_resolutionBW()))
         ms2830a.do_set_videoBW(100)
         print("Visual bandwith:" + str(ms2830a.do_get_videoBW))
         
         # AMPLITUDE
-        ms2830a.set_amplitude_scale(1)
+        ms2830a.set_amplitude_scale(config_file["amplitude_log_scale"])
         print("Amplitude log scale: " + str(ms2830a.get_amplitude_scale()))
-        ms2830a.set_reference_level(-62)
+        ms2830a.set_reference_level(config_file["reference_level"])
         print("Reference level: " + str(ms2830a.get_reference_level()))
 
         # MARKER
-        ms2830a.set_zoom_spot_marker(1, "SPOT")
+        ms2830a.set_zoom_spot_marker(config_file["zoom_spot_marker"][0], config_file["zoom_spot_marker"][1])
 
 
 def plot_lineplot(trace):
@@ -55,3 +55,13 @@ def save_data_as_csv(trace):
         dataset = pd.DataFrame(trace)
         dataset.to_csv("../data/measure-" + str(datetime.datetime.now()) + ".csv")
 
+def read_config_file(filename):
+        data = []
+        with open(filename, 'r') as file:
+                data = file.read()
+        return json.loads(data)
+
+def write_config_file(filename, confDict):
+        json_obj = json.dumps(confDict, indent=4)
+        with open(filename, 'w') as file:
+                file.write(json_obj)
--- src/main.py
+++ src/main.py
@@ -6,9 +6,10 @@ if __name__ == '__main__':
     gpib = "GPIB::18::INSTR"
     remote_eth = "TCPIP0::localhost::49153::SOCKET"
     #local_eth =
+    config_file = Utils.read_config_file("../config/config_MS2830A.json")
     
     instr = SPA.Anritsu_MS2830A("Anritsu_MS2830A", remote_eth)
-    Utils.set_SPA_for_measure(instr)
+    Utils.set_SPA_for_measure(instr, config_file)
 
     trace = instr.get_trace(1) # Get trace
     
